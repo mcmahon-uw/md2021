8922b25ec6cb00d1dde567b01c0259c32e5419a9
Trina McMahon <trina.mcmahon@wisc.edu>
2021-07-13 14:33:38 -0400

    typo

--- code/spades_assembly.sh
+++ code/spades_assembly.sh
@@ -17,4 +17,4 @@ outdir=~/md2021/analysis/CedarSwamp
 SPADESPATH=/opt/bifxapps/SPAdes-3.9.0-Linux/bin/
 
 # Run spades
-/opt/bifxapps/bin/python3.4 $SPADESPATH/spades.py -t 20 -m 800 -meta -k 21,33,55,77,99,127 -1 $r1 -2 $r2 -o $outdir/$sample
+/opt/bifxapps/bin/python3.4 $SPADESPATH/spades.py -t 20 -m 800 --meta -k 21,33,55,77,99,127 -1 $r1 -2 $r2 -o $outdir/$sample
